70f1c832936746051ed61cf69946f302add19391
Joseph Chen <Joseph.Chen@nike.com>
2025-07-08 14:45:44 +0800

    first commit

---
+++ src/main/java/com/example/springcc/SpringCcApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCcApplication.class, args);
+    }
+
+}
